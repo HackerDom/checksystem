0ca81eace2c821c5ad2d4947d0b759d1d29f1519
Andrey Khozov <avkhozov@gmail.com>
2015-04-18 20:56:02 +0500

    Show only last 30 rounds in admin page

--- lib/CS/Controller/Admin.pm
+++ lib/CS/Controller/Admin.pm
@@ -11,10 +11,10 @@ sub index {
 sub view {
   my $c = shift;
 
-  my $db     = $c->pg->db;
+  my $db   = $c->pg->db;
   my $view = $db->query(
-    'select round, status, result from runs where team_id = ? and service_id = ? order by round desc',
-    $c->param('team_id'),
+    'select round, status, result
+    from runs where team_id = ? and service_id = ? order by round desc limit 30', $c->param('team_id'),
     $c->param('service_id')
   )->expand->hashes->to_array;
   $c->render(view => $view);
