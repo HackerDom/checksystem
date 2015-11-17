29e9b66d7874d87abadc0f1f24249a7cdf9dd9a0
Andrey Khozov <avkhozov@gmail.com>
2015-11-17 10:48:23 +0500

    Set cwd for checkers

--- lib/CS/Model/Checker.pm
+++ lib/CS/Model/Checker.pm
@@ -2,6 +2,7 @@ package CS::Model::Checker;
 use Mojo::Base 'MojoX::Model';
 
 use Convert::Color;
+use File::Spec;
 use IPC::Run qw/run timeout/;
 use List::Util 'all';
 use Mojo::Util 'trim';
@@ -47,30 +48,33 @@ sub check {
   return $self->_finish($job, $result)
     unless $round == $job->app->pg->db->query('select max(n) from rounds')->array->[0];
 
+  my $path = File::Spec->rel2abs($service->{path});
+  my (undef, $cwd) = File::Spec->splitpath($path);
+
   my $host = $team->{host};
   if (my $cb = $job->app->config->{cs}{checkers}{hostname}) { $host = $cb->($team, $service) }
 
   # Check
-  my $cmd = [$service->{path}, 'check', $host];
-  $result->{check} = $self->_run($cmd, $service->{timeout});
+  my $cmd = [$path, 'check', $host];
+  $result->{check} = $self->_run($cmd, $service->{timeout}, $cwd);
   return $self->_finish($job, $result) unless $result->{check}{exit_code} == 101;
 
   # Put
-  $cmd = [$service->{path}, 'put', $host, $flag->{id}, $flag->{data}, $vuln->{n}];
-  $result->{put} = $self->_run($cmd, $service->{timeout});
+  $cmd = [$path, 'put', $host, $flag->{id}, $flag->{data}, $vuln->{n}];
+  $result->{put} = $self->_run($cmd, $service->{timeout}, $cwd);
   (my $id = $result->{put}{stdout}) =~ s/\r?\n$//;
   $flag->{id} = $result->{put}{fid} = $id if $id;
   return $self->_finish($job, $result) unless $result->{put}{exit_code} == 101;
 
   # Get 1
-  $cmd = [$service->{path}, 'get', $host, $flag->{id}, $flag->{data}, $vuln->{n}];
-  $result->{get_1} = $self->_run($cmd, $service->{timeout});
+  $cmd = [$path, 'get', $host, $flag->{id}, $flag->{data}, $vuln->{n}];
+  $result->{get_1} = $self->_run($cmd, $service->{timeout}, $cwd);
   return $self->_finish($job, $result) unless $result->{get_1}{exit_code} == 101;
 
   # Get 2
   if ($old_flag) {
-    $cmd = [$service->{path}, 'get', $host, $old_flag->{id}, $old_flag->{data}, $vuln->{n}];
-    $result->{get_2} = $self->_run($cmd, $service->{timeout});
+    $cmd = [$path, 'get', $host, $old_flag->{id}, $old_flag->{data}, $vuln->{n}];
+    $result->{get_2} = $self->_run($cmd, $service->{timeout}, $cwd);
   }
   return $self->_finish($job, $result);
 }
@@ -83,12 +87,16 @@ sub _finish {
 }
 
 sub _run {
-  my ($self, $cmd, $timeout) = @_;
+  my ($self, $cmd, $timeout, $cwd) = @_;
   my ($stdout, $stderr);
 
   $self->app->log->debug("Run '@$cmd' with timeout $timeout");
   my $start = [gettimeofday];
-  eval { run $cmd, \undef, \$stdout, \$stderr, timeout($timeout) };
+  eval {
+    run $cmd, \undef, \$stdout, \$stderr,
+      init => sub { chdir $cwd },
+      timeout($timeout);
+  };
   my $elapsed = tv_interval($start);
 
   $timeout = ($@ && $@ =~ /timeout/i) ? 1 : 0;
