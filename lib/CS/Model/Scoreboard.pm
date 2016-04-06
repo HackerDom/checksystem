07acd637e9e93368d1ce4ed2b94a887cae7ce1a3
Andrey Khozov <avkhozov@gmail.com>
2016-04-06 09:42:05 +0500

    Fixed team name and host in scoreboard

--- lib/CS/Model/Scoreboard.pm
+++ lib/CS/Model/Scoreboard.pm
@@ -7,12 +7,13 @@ sub generate {
   my $self = shift;
   my $db   = $self->app->pg->db;
 
-  my $scoreboard =
-    $db->query('select * from scoreboard where round = (select max(round) from scoreboard) order by n')
-    ->expand->hashes;
+  my $scoreboard = $db->query(
+    'select * from scoreboard as s join teams as t on s.team_id = t.id
+      where round = (select max(round) from scoreboard) order by n'
+  )->expand->hashes;
+  my $round = $db->query('select max(n) from rounds')->array->[0];
 
-  return (
-    {scoreboard => $scoreboard->to_array, round => $db->query('select max(n) from rounds')->array->[0]});
+  return ({scoreboard => $scoreboard->to_array, round => $round});
 }
 
 1;
