bea80189d00bf919c813263f2bd563b6d66d479a
Andrey Khozov <avkhozov@gmail.com>
2016-04-11 09:44:38 +0500

    Fixed bug with detect current round

--- lib/CS/Model/Scoreboard.pm
+++ lib/CS/Model/Scoreboard.pm
@@ -2,14 +2,15 @@ package CS::Model::Scoreboard;
 use Mojo::Base 'MojoX::Model';
 
 sub generate {
-  my $self = shift;
-  my $db   = $self->app->pg->db;
+  my ($self, $round) = @_;
+  my $db = $self->app->pg->db;
 
+  $round //= $db->query('select max(round) from scoreboard')->array->[0];
   my $scoreboard = $db->query(
     'select t.host, t.name, s.* from scoreboard as s join teams as t on s.team_id = t.id
-      where round = (select max(round) from scoreboard) order by n'
+      where round = ? order by n', $round
   )->expand->hashes;
-  my $round = $db->query('select max(n) from scoreboard')->array->[0];
+
 
   return {scoreboard => $scoreboard->to_array, round => $round};
 }
