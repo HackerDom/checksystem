25f94d84e1d872cb047bb5fa4533d09ae76bf661
Andrey Khozov <avkhozov@gmail.com>
2016-04-13 13:24:46 +0500

    Improved charts_data action

--- lib/CS/Controller/Main.pm
+++ lib/CS/Controller/Main.pm
@@ -4,25 +4,32 @@ use Mojo::Base 'Mojolicious::Controller';
 sub index { $_[0]->render(%{$_[0]->model('scoreboard')->generate}) }
 
 sub charts_data {
-  my $c = shift;
+  my $c  = shift;
+  my $pg = $c->pg;
 
-  my $db = $c->pg->db;
-  my $tx = $db->begin;
-  $db->query('set transaction isolation level repeatable read');
-  my $scores = $db->query(
-    'select team_id as name, array_agg(score order by round) as data from scoreboard group by team_id')
-    ->expand->hashes;
-  my $rounds = $db->query('select distinct(round) from scoreboard order by 1')->arrays->flatten;
-  my $flags  = $db->query(
-    q{select service_id as name, array_agg(flags order by round)::int[] as data
-from
-(select round, (service->>'id')::int as service_id, sum((service->>'flags')::int) as flags
-  from (select round, team_id, json_array_elements(services) as service from scoreboard) as s
-group by 1, 2) as ss
-group by service_id}
-  )->expand->hashes;
-
-  $c->render(json => {rounds => $rounds, scores => $scores, flags => $flags});
+  $c->delay(
+    sub {
+      my $delay = shift;
+      $pg->db->query(
+        'select team_id as name, array_agg(score order by round) as data from scoreboard group by team_id',
+        $delay->begin);
+      $pg->db->query('select distinct(round) from scoreboard order by 1', $delay->begin);
+      $pg->db->query('
+        select service_id as name, array_agg(flags order by round)::int[] as data
+        from (select round, service_id, sum(flags) as flags from scores group by round, service_id) as s
+        group by service_id', $delay->begin);
+    },
+    sub {
+      my ($delay, undef, $scores, undef, $rounds, undef, $flags) = @_;
+      $c->render(
+        json => {
+          rounds => $rounds->arrays->flatten,
+          scores => $scores->expand->hashes,
+          flags  => $flags->expand->hashes
+        }
+      );
+    }
+  );
 }
 
 sub scoreboard {
