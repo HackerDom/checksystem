638dea57b0e1a2c36ac251789f4971c49c655891
Andrey Khozov <avkhozov@gmail.com>
2017-11-05 23:42:48 +0500

    use values instead of tags for rounds in influxdb

--- lib/CS/Command/manager.pm
+++ lib/CS/Command/manager.pm
@@ -47,7 +47,7 @@ sub start_round {
   $self->round($round);
   $app->minion->enqueue('scoreboard');
   $app->log->info("Start new round #$round");
-  $app->metric->write('round', 1, {n => $round});
+  $app->metric->write('round', $round);
 
   my $status = $db->query(
     'select distinct on (team_id, service_id) *
