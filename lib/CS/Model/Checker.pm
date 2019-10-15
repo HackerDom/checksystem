5804e21327873fbeab99a1f6d6f96415b946f247
Andrey Khozov <avkhozov@gmail.com>
2019-10-15 06:22:17 +0500

    fix bug while detect actual state

--- lib/CS/Model/Checker.pm
+++ lib/CS/Model/Checker.pm
@@ -97,7 +97,10 @@ sub _finish {
     $result->{error} = 'Job is too old!';
     $status = 104;
   } else {
-    my $state = c(qw/get_2 get_1 put check/)->grep(sub { defined $result->{$_}{exit_code} })->sort->last;
+    my $state = c(qw/get_2 get_1 put check/)
+      ->grep(sub { defined $result->{$_}{exit_code} })
+      ->sort(sub { $result->{$a}{exit_code} <=> $result->{$b}{exit_code} })
+      ->last;
     $status = $result->{$state}{exit_code};
     $stdout = $result->{$state}{stdout} if $status != 101;
   }
